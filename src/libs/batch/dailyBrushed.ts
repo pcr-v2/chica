7af451e8ff20c9db002bcbf52fe5c7ff698b2948
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-30 14:59:57 +0900

    배치테스트 #5

--- src/libs/batch/dailyBrushed.ts
+++ src/libs/batch/dailyBrushed.ts
@@ -1,49 +1,15 @@
 // lib/batch/dailyBrushed.ts
-import { jwtVerify } from "jose";
-import { cookies } from "next/headers";
 import cron from "node-cron";
 
 import { mysqlPrisma } from "@/libs/prisma";
 
-const secretKey = new TextEncoder().encode(process.env.TOKEN_SECRET);
-
 // 매일 오전 6시 (서버 시간 기준) 실행
-cron.schedule("35 5 * * *", async () => {
+cron.schedule("10 6 * * *", async () => {
   const today = new Date().toISOString().split("T")[0]; // YYYY-MM-DD 기준
   //   const brushedAt = new Date(`${today}T06:00:00+09:00`);
 
   try {
-    const cookieStore = await cookies();
-
-    const accessToken = cookieStore.get("CHICA_USER_ACCESS_TOKEN")?.value;
-
-    if (accessToken == null) {
-      return {
-        code: "UNAUTHORIZED",
-        message: "인증이 필요합니다.",
-      };
-    }
-
-    const verified = await jwtVerify(accessToken, secretKey);
-
-    const admin = await mysqlPrisma.school.findFirst({
-      where: {
-        loginId: verified.payload.id!,
-      },
-    });
-
-    if (admin == null) {
-      return {
-        code: "NOT_FOUND",
-        message: "존재하지 않는 유저입니다.",
-      };
-    }
-
-    const students = await mysqlPrisma.student.findMany({
-      where: {
-        schoolId: admin.schoolId,
-      },
-    });
+    const students = await mysqlPrisma.student.findMany({});
 
     const insertData = students.map((student) => ({
       studentId: student.studentId,
@@ -56,7 +22,7 @@ cron.schedule("35 5 * * *", async () => {
     });
 
     console.log(
-      `[Batch] ${insertData.length} brushed rows inserted at ${new Date()}`,
+      `[Batch] ${insertData.length} brushed rows inserted at ${today}`,
     );
   } catch (err) {
     console.error("[Batch Error] brushed insert failed:", err);
