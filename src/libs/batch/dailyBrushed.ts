8504c7b37cbe7ba32d748d8c92e31a524aba99fa
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-30 15:40:11 +0900

    배치테스트 #7

--- src/libs/batch/dailyBrushed.ts
+++ src/libs/batch/dailyBrushed.ts
@@ -1,10 +1,10 @@
 // lib/batch/dailyBrushed.ts
 import cron from "node-cron";
 
-import { mysqlPrisma } from "@/libs/prisma";
+import { mysqlPrisma } from "../../../src/libs/prisma";
 
 // 매일 오전 6시 (서버 시간 기준) 실행
-cron.schedule("10 6 * * *", async () => {
+cron.schedule("45 6 * * *", async () => {
   const today = new Date().toISOString().split("T")[0]; // YYYY-MM-DD 기준
   //   const brushedAt = new Date(`${today}T06:00:00+09:00`);
 
