86d43ed066f8c857398211056e9f7e7199b82b02
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-31 14:17:04 +0900

    추가 수정

--- src/app/(main)/summary/page.tsx
+++ src/app/(main)/summary/page.tsx
@@ -1,15 +1,13 @@
 import SummaryContainer from "@/app/(main)/summary/SummaryContainer";
-import { getStudentSummary } from "@/app/actions/student/getStudentAction";
 import { getUnChecked } from "@/app/actions/summary/getUnCheckedAction";
 
-interface IProps {
+interface Props {
   searchParams: {
     studentId?: string;
   };
 }
-
-export default async function page(props: IProps) {
-  const { studentId } = props.searchParams;
+export default async function page({ searchParams }: Props) {
+  const { studentId } = searchParams;
   console.log("studentId", studentId);
 
   const unCheckedList = await getUnChecked({ studentId: studentId as string });
