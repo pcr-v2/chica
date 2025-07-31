4159fc024285934d7d5806b3590b19cb53d09ae0
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-31 14:41:54 +0900

    서치파람 비동기로 수정

--- src/app/(main)/summary/page.tsx
+++ src/app/(main)/summary/page.tsx
@@ -1,16 +1,16 @@
 import SummaryContainer from "@/app/(main)/summary/SummaryContainer";
 import { getUnChecked } from "@/app/actions/summary/getUnCheckedAction";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: Record<string, string | string[] | undefined>;
-}) {
-  const studentId =
-    typeof searchParams?.studentId === "string"
-      ? searchParams.studentId
-      : undefined;
-  const unCheckedList = await getUnChecked({ studentId: studentId ?? "" });
+interface ISearchParams {
+  searchParams: Promise<{ studentId?: string }>;
+}
+export default async function page({ searchParams }: ISearchParams) {
+  //  searchParams;
 
+  const params = await searchParams;
+
+  const unCheckedList = await getUnChecked({
+    studentId: params.studentId as string,
+  });
   return <SummaryContainer unCheckedList={unCheckedList} />;
 }
