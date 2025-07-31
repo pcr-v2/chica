5d9dc7aeb479fe5bffc4656d174647ead2994153
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-31 14:26:12 +0900

    타입 충돌 수정

--- src/app/(main)/summary/page.tsx
+++ src/app/(main)/summary/page.tsx
@@ -1,14 +1,15 @@
 import SummaryContainer from "@/app/(main)/summary/SummaryContainer";
 import { getUnChecked } from "@/app/actions/summary/getUnCheckedAction";
 
-interface PageProps {
-  searchParams?: {
-    studentId?: string;
-  };
-}
-
-export default async function Page({ searchParams }: PageProps) {
-  const studentId = searchParams?.studentId;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: Record<string, string | string[] | undefined>;
+}) {
+  const studentId =
+    typeof searchParams?.studentId === "string"
+      ? searchParams.studentId
+      : undefined;
   const unCheckedList = await getUnChecked({ studentId: studentId ?? "" });
 
   return <SummaryContainer unCheckedList={unCheckedList} />;
