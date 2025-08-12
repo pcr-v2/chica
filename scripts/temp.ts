9a247b0267f3b8a0ca454789378ae812a348f810
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-08-12 19:32:06 +0900

    배치 테스트 1

---
+++ scripts/temp.ts
@@ -0,0 +1,42 @@
+// // dailyBrushed.ts
+// import "dotenv/config";
+
+// import { mysqlPrisma } from "../src/libs/prisma";
+
+// async function main() {
+//   const today = new Date().toISOString().split("T")[0];
+//   try {
+//     const students = await mysqlPrisma.student.findMany({
+//       where: {
+//         studentStatus: true,
+//       },
+//     });
+
+//     const insertData = students.map((student) => ({
+//       studentId: student.studentId,
+//       brushedStatus: "No",
+//       brushedAt: new Date().toISOString(),
+//     }));
+
+//     await mysqlPrisma.brushed.createMany({
+//       data: insertData.map((el) => ({
+//         brushedAt: el.brushedAt,
+//         brushedStatus: el.brushedStatus as
+//           | "No"
+//           | "Ok"
+//           | "EarlyLeave"
+//           | "Travel"
+//           | "Workshop"
+//           | "Absence",
+//         studentId: el.studentId,
+//       })),
+//       skipDuplicates: true,
+//     });
+
+//     console.log([Batch] ${insertData.length} rows inserted at ${today});
+//   } catch (err) {
+//     console.error("[Batch Error] brushed insert failed:", err);
+//   }
+// }
+
+// main();
